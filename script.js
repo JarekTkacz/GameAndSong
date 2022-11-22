d4cff4f0aa8355413f10de141ebd3c1995ad1ec3
JarekTkacz <116753118+JarekTkacz@users.noreply.github.com>
2022-11-22 13:55:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -74,16 +74,3 @@ function beer(){
 			}
 }
 
-/*for(int i=99; i>=1; i--){
-			if(i>=2){
-				System.out.println(+i+" bottles of beer on the wall, "+i+" bottles of beer");
-				System.out.println("Take one down and pass it around, "+(i-1)+" bottles of beer on the wall");
-			}
-			else{
-			System.out.println("1 bottle of beer on the wall, 1 bottle of beer");
-			System.out.println("Take one down and pass it around, no more bottles of beer on the wall");
-			System.out.println("No more bottles of beer on the wall, no more bottles of beer");
-			System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall");
-				}
-
-			}*/
